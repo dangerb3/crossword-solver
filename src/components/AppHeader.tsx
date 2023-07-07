6c8e0b2e9eea3a470309bf6995c7a8f19c3b543c
Daniil <dangerb@yandex.ru>
2023-07-08 01:42:25 +0600

    Styles improved

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -15,42 +15,40 @@ export const AppHeader = memo(({ changeLanguage, currentLanguage }: AppHeaderPro
   //TODO: add version auto updating
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <AppBar component="nav">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'block', sm: 'block' } }}
+    <AppBar component="nav">
+      <Toolbar>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, display: { xs: 'block', sm: 'block' } }}
+        >
+          Crossword Solver
+        </Typography> {/* TODO: LOGO CENTER */}
+        <Box component='nav'>
+          <Select
+            labelId="select-lang-label"
+            id="select-lang"
+            size='small'
+            variant='outlined'
+            sx={{ border: '0px' }}
+            value={currentLanguage ? LanguagesStructure.get(currentLanguage) : languageObjectsArray[0]}
+            onChange={(e) => changeLanguage(e.target.value as Languages)}
+            renderValue={(lang) => (
+              <Box sx={{ display: 'flex', alignItems: 'center', paddingRight: '15px' }}>
+                <img src={lang.imgPath} style={{ marginRight: '5px' }} alt='flag' />
+                {lang.value}
+              </Box>
+            )}
           >
-            Crossword Solver
-          </Typography> {/* TODO: LOGO CENTER */}
-          <Box component='nav'>
-            <Select
-              labelId="select-lang-label"
-              id="select-lang"
-              size='small'
-              variant='outlined'
-              sx={{ border: '0px' }}
-              value={currentLanguage ? LanguagesStructure.get(currentLanguage) : languageObjectsArray[0]}
-              onChange={(e) => changeLanguage(e.target.value as Languages)}
-              renderValue={(lang) => (
-                <Box sx={{ display: 'flex', alignItems: 'center', paddingRight: '15px' }}>
-                  <img src={lang.imgPath} style={{ marginRight: '5px' }} alt='flag' />
-                  {lang.value}
-                </Box>
-              )}
-            >
-              {languageObjectsArray.map((lang, index) => ( // TODO: add menu: lang, theme change and about
-                <MenuItem key={index} value={languageNamesArray[index]}>
-                  <img src={lang.imgPath} width='15px' height='15px' style={{ marginRight: '5px' }} alt='flag' />
-                  <ListItemText primary={lang.value} />
-                </MenuItem>
-              ))}
-            </Select>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+            {languageObjectsArray.map((lang, index) => ( // TODO: add menu: lang, theme change and about
+              <MenuItem key={index} value={languageNamesArray[index]}>
+                <img src={lang.imgPath} width='15px' height='15px' style={{ marginRight: '5px' }} alt='flag' />
+                <ListItemText primary={lang.value} />
+              </MenuItem>
+            ))}
+          </Select>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 })
